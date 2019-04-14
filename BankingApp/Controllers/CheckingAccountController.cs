1545f77468f73604f784e341de29de316c422bbf
bonnina <37539513+bonnina@users.noreply.github.com>
2019-04-14 22:45:04 +0300

    Create CheckingAccount controller

---
+++ BankingApp/Controllers/CheckingAccountController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BankingApp.Models;
+
+namespace BankingApp.Controllers
+{
+    public class CheckingAccountController : Controller
+    {
+        // GET: CheckingAccount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CheckingAccount/Details/5
+        public ActionResult Details()
+        {
+            var checkingAccount = new CheckingAccount
+            {
+                AccountNumber = "0000111122223333",
+                FirstName = "John",
+                LastName = "Doe",
+                Balance = 500
+            };
+            return View(checkingAccount);
+        }
+
+        // GET: CheckingAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckingAccount/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckingAccount/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
