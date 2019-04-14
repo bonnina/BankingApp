e40adfb76c0c23aa6c025205abbb77d7c5d3e9f4
bonnina <37539513+bonnina@users.noreply.github.com>
2019-04-14 22:43:50 +0300

    Create CheckingAccount model

---
+++ BankingApp/Models/CheckingAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankingApp.Models
+{
+    public class CheckingAccount
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Account №")]
+        public string AccountNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Name {
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            } }
+
+        [DataType(DataType.Currency)]
+        public decimal Balance { get; set; }
+    }
+}
