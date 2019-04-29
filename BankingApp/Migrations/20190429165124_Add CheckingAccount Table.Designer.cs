f558a0d6c0a202131ccb3428b0bd37ead0f35ba4
bonnina <37539513+bonnina@users.noreply.github.com>
2019-04-29 20:11:43 +0300

    Apply migration

--- BankingApp/Migrations/20190429165124_Add CheckingAccount Table.Designer.cs
+++ BankingApp/Migrations/20190429165124_Add CheckingAccount Table.Designer.cs
@@ -4,14 +4,16 @@ using BankingApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BankingApp.Migrations
 {
     [DbContext(typeof(BankingAppContext))]
-    partial class BankingAppContextModelSnapshot : ModelSnapshot
+    [Migration("20190429165124_Add CheckingAccount Table")]
+    partial class AddCheckingAccountTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BankingApp/Migrations/20190429165124_Add CheckingAccount Table.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankingApp.Migrations
+{
+    public partial class AddCheckingAccountTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CheckingAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(maxLength: 10, nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AccountNumber = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Balance = table.Column<decimal>(nullable: false),
+                    BankingAppUserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckingAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CheckingAccounts_AspNetUsers_BankingAppUserId",
+                        column: x => x.BankingAppUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CheckingAccounts_BankingAppUserId",
+                table: "CheckingAccounts",
+                column: "BankingAppUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckingAccounts");
+        }
+    }
+}
