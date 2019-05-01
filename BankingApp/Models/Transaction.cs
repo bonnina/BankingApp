4f6603c4303a1ddea7f3431508ee0d394780850d
bonnina <37539513+bonnina@users.noreply.github.com>
2019-05-01 15:32:27 +0300

    Create Transaction model

---
+++ BankingApp/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankingApp.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int CheckingAccountId { get; set; }
+        
+        public virtual CheckingAccount CheckingAccount { get; set; }
+    }
+}
