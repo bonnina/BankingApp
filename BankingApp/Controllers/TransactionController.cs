9387d35b619656d4a6aa1576c636de865dd2c24c
bonnina <37539513+bonnina@users.noreply.github.com>
2019-05-01 15:33:12 +0300

    Add Trnsaction Controller

---
+++ BankingApp/Controllers/TransactionController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankingApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BankingApp.Controllers
+{
+    [Authorize]
+    public class TransactionController : Controller
+    {
+        private readonly BankingAppContext _context;
+
+        // GET: Transaction/Deposit
+        public IActionResult Deposit(int CheckingAccountId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Deposit(Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Transactions.Add(transaction);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
