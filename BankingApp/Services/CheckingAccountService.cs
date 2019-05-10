35ac3affa46f0bbbfd0868c0577b2019a72afa42
bonnina <37539513+bonnina@users.noreply.github.com>
2019-05-10 23:13:45 +0300

    Add service to avoid repetitions

---
+++ BankingApp/Services/CheckingAccountService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using BankingApp.Models;
+
+namespace BankingApp.Services
+{
+    public class CheckingAccountService
+    {
+        private readonly BankingAppContext _context;
+
+        public CheckingAccountService(BankingAppContext context)
+        {
+            _context = context;
+        }
+
+        public async void CreateCheckingAccount(string firstName, string lastName, string userId, 
+            decimal initialBalance = 0)
+        {
+            var accountNumber = (11112222 + await _context.CheckingAccounts.CountAsync())
+                           .ToString()
+                           .PadLeft(10, '0');
+
+            var checkingAccount = new CheckingAccount
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                AccountNumber = accountNumber,
+                Balance = initialBalance,
+                BankingAppUserId = userId
+            };
+
+            _context.CheckingAccounts.Add(checkingAccount);
+                        _context.SaveChanges();
+        }
+    }
+}
