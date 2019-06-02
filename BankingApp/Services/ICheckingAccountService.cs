8cd24bdfe0a4f6b4f5120141f8e92b0a90df1817
bonnina <37539513+bonnina@users.noreply.github.com>
2019-06-02 20:04:20 +0300

    Add ICheckingAccountService for Tasks

---
+++ BankingApp/Services/ICheckingAccountService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace BankingApp.Services
+{
+    public interface ICheckingAccountService
+    {
+        Task CreateCheckingAccount(string firstName, string lastName, string userId, decimal initialBalance = 0);
+
+        Task UpdateBalance(int checkingAccountId);
+    }
+}
