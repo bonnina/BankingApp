aa07a24494e3b1bd26b54d0b2872ee26a8d55221
bonnina <37539513+bonnina@users.noreply.github.com>
2019-06-03 07:27:53 +0300

    Inject Services

--- BankingApp/Startup.cs
+++ BankingApp/Startup.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using BankingApp.Areas.Identity.Data;
 using BankingApp.Models;
+using BankingApp.Services;
 
 namespace BankingApp
 {
@@ -38,8 +35,15 @@ namespace BankingApp
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
+            #region Dependency Injection
+
+            services.AddScoped<ICheckingAccountService, CheckingAccountService>();
+
+            #endregion
+
             services.AddDefaultIdentity<BankingAppUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<BankingAppContext>();
 
             services.Configure<IdentityOptions>(options =>
