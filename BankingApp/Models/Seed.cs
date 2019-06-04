4d192551581393b33c5d21531f8d2ea31cae8bd6
bonnina <37539513+bonnina@users.noreply.github.com>
2019-06-04 23:14:51 +0300

    Update Seep: inject checkingAccountService

--- BankingApp/Models/Seed.cs
+++ BankingApp/Models/Seed.cs
@@ -12,29 +12,32 @@ namespace BankingApp.Models
     public class Seed
     {
         private readonly UserManager<BankingAppUser> _userManager;
+        private readonly ICheckingAccountService _checkingAccountService;
 
-        public Seed (UserManager<BankingAppUser> userManager)
+        public Seed (UserManager<BankingAppUser> userManager, ICheckingAccountService checkingAccountService)
         {
-             _userManager = userManager;
+            _userManager = userManager;
+            _checkingAccountService = checkingAccountService;
         }
 
         public async Task Initialize(IServiceProvider serviceProvider)
         {
             using (var context = new BankingAppContext(
-               serviceProvider.GetRequiredService<DbContextOptions<BankingAppContext>>()))
+                serviceProvider.GetRequiredService<DbContextOptions<BankingAppContext>>()))
             {
                 if (!context.Users.Any(u => u.UserName == "admin@gmail.com"))
                 {
-                    var user = new BankingAppUser {
+                    var user = new BankingAppUser
+                    {
                         UserName = "admin@gmail.com",
-                        Email = "admin@gmail.com"};
+                        Email = "admin@gmail.com"
+                    };
 
                     await _userManager.CreateAsync(user, "passW0rd");
 
-                    var service = new CheckingAccountService(context);
-                    service.CreateCheckingAccount("admin", "user", user.Id, 1000);
+                    await _checkingAccountService.CreateCheckingAccount("admin", "user", user.Id, 1000);
 
-                    context.Roles.Add(new IdentityRole { Name = "Admin" });
+                    context.Roles.Add(new IdentityRole {Name = "Admin"});
                     context.SaveChanges();
 
                     await _userManager.AddToRoleAsync(user, "Admin");
