419730ef74a127c79ad20df25e78e41381e5a742
bonnina <37539513+bonnina@users.noreply.github.com>
2019-07-20 14:44:10 +0300

    Update Tranfer

--- BankingApp/Controllers/TransactionController.cs
+++ BankingApp/Controllers/TransactionController.cs
@@ -92,7 +92,7 @@ namespace BankingApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Transfer(string toAccount, decimal amount)
+        public async Task<IActionResult> Transfer(string accountNumber, decimal amount)
         {
             var user = _userManager.FindByIdAsync(User.Identity.Name);
             var userId = _userManager.GetUserId(HttpContext.User);
@@ -109,7 +109,7 @@ namespace BankingApp.Controllers
                 {
                     await _transactionManager.CreateTransaction(-System.Math.Abs(amount), userId);
 
-                    await _transactionManager.CreateTransaction(amount, toAccount);
+                    await _transactionManager.CreateTransaction(System.Math.Abs(amount), accountNumber);
 
                     return RedirectToAction("Index", "Home");
                 }
