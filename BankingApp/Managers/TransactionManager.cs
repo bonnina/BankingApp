4cccc991ae58eb7e5abc20ae7f76e0251f367ba2
bonnina <37539513+bonnina@users.noreply.github.com>
2019-07-27 22:57:41 +0300

    Implement TransferFunds (transactional way)

--- BankingApp/Managers/TransactionManager.cs
+++ BankingApp/Managers/TransactionManager.cs
@@ -3,6 +3,7 @@ using BankingApp.Models;
 using BankingApp.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BankingApp.Managers
 {
@@ -41,6 +42,43 @@ namespace BankingApp.Managers
             await _context.SaveChangesAsync();
 
             await _checkingAccountManager.UpdateBalance(transaction.CheckingAccountId);
+        }
+
+        public async Task TransferFunds(decimal amount, string userId, int checkingAccountId)
+        {
+            CheckingAccount senderAccount = await _context.CheckingAccounts
+                .FirstAsync(c => c.BankingAppUserId == userId);
+
+            var senderTransaction = new Transaction
+            {
+                Amount = -System.Math.Abs(amount),
+                CheckingAccountId = senderAccount.Id,
+                CheckingAccount = senderAccount
+            };
+
+            _context.Transactions.Add(senderTransaction);
+
+            senderAccount.Balance = await _context.Transactions
+                .Where(c => c.CheckingAccountId == senderAccount.Id)
+                .SumAsync(c => c.Amount);
+            
+            CheckingAccount recipientAccount = await _context.CheckingAccounts
+                .FirstAsync(c => c.Id == checkingAccountId);
+
+            var recipientTransaction = new Transaction
+            {
+                Amount = System.Math.Abs(amount),
+                CheckingAccountId = recipientAccount.Id,
+                CheckingAccount = recipientAccount
+            };
+
+            _context.Transactions.Add(recipientTransaction);
+
+            recipientAccount.Balance = await _context.Transactions
+                .Where(c => c.CheckingAccountId == recipientAccount.Id)
+                .SumAsync(c => c.Amount);
+
+            await _context.SaveChangesAsync();
         }
     }
 }
